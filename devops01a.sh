168410f31dfca53448307692894ef994753de74a
vhari <vhari.devops@gmail.com>
2019-05-17 16:59:21 -0700

    Bash script with List of volumes

--- devops01a.sh
+++ devops01a.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 echo "First Program"
+# 1. number of volumes, size of each volume, free space on each volume
+echo
+echo "***************To get number of volumes, size of each volume, free space on each volume" 
+df -a
+echo
+echo "***************In Readable format*****************"
+echo
+df -h
+echo "--------------------------------------------------"
