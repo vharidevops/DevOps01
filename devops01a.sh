f512d2c220768f1c66dd63ff8c0c334a891a9873
vhari <vhari.devops@gmail.com>
2019-06-14 14:26:00 -0700

    Added cmds

--- devops01a.sh
+++ devops01a.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 echo "First Program"
-# 1. number of volumes, size of each volume, free space on each volume
+# Part 2
+# A. number of volumes, size of each volume, free space on each volume
 echo
-echo "***************To get number of volumes, size of each volume, free space on each volume" 
+#echo "***************To get number of volumes, size of each volume, free space on each volume" 
 df -a
 echo
 echo "***************In Readable format*****************"
 echo
 df -h
 echo "--------------------------------------------------"
+
+# B. number cpus/cores, information about the cpus/core
+echo "*** Number cpus/cores, information about the cpus/core ***"
+cat /proc/cpuinfo
+echo
+
+# C. amount of ram
+echo "*** amount of ram ***"
+echo
+free -g
+echo
+echo "***************In Readable format*****************"
+echo
+free -m
+echo
